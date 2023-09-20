2d19de6661b775298537115c2439f4525d362d79
habiba kamoun <kamounhabiba@gmail.com>
2023-09-20 16:17:57 +0100

    commit

---
+++ traitement.php
@@ -0,0 +1,16 @@
+<?php
+$id=$_GET['id']; 
+$nom=$_GET['nom'];
+$desc=$_GET['desc'];
+$adresse=$_GET['adresse'];
+$domaine=$_GET['domaine'];
+echo $id;
+echo '<br>';
+echo $nom;
+echo '<br>';
+echo $desc;
+echo '<br>';
+echo $adresse;
+echo '<br>';
+echo $domaine;
+?>
